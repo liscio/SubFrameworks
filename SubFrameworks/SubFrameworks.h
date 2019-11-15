e8de354b071628aeb1b3381c34a4c2462d317620
Christopher Liscio <chris@supermegaultragroovy.com>
2019-11-15 17:20:07 -0500

    Initial Commit

---
+++ SubFrameworks/SubFrameworks.h
@@ -0,0 +1,19 @@
+//
+//  SubFrameworks.h
+//  SubFrameworks
+//
+//  Created by Christopher Liscio on 2019-11-15.
+//  Copyright © 2019 Christopher Liscio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubFrameworks.
+FOUNDATION_EXPORT double SubFrameworksVersionNumber;
+
+//! Project version string for SubFrameworks.
+FOUNDATION_EXPORT const unsigned char SubFrameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubFrameworks/PublicHeader.h>
+
+
